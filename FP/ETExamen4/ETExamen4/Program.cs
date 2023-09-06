e4c7fd56519ea8fc5b7a2c188c0defa880112255
66Estef <lintzyzep@gmail.com>
2023-09-05 21:03:04 -0600

    ECUACIÓN
    Solution exercise of a quadratic equation with double and mathematical functions in C#

---
+++ FP/ETExamen4/ETExamen4/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+//GARCIA_ZEPEDA_040422
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Ejercicios Tipo Exámen 4: Ecuación de segundo grado");
+
+//3 Variables double para numeros con decimales o no enteros
+double a, b, c;
+a = 6;
+b = -5;
+c = 1;
+
+//2 Variables para las 2 alternativas de la ecuación
+double X1, X2;
+
+Console.WriteLine("Ecuación a resolver: 6x^2 - 5x + 1 = 0");
+//Procedimiento
+X1 = - b + (Math.Sqrt(Math.Pow(b, 2) - (4 * a * c)) / (2 * a));
+X2 = - b - (Math.Sqrt(Math.Pow(b, 2) - (4 * a * c)) / (2 * a));
+
+//Respuesta
+Console.WriteLine(X1);
+Console.WriteLine(X2);
