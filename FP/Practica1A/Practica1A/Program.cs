fe306e493675e6dbddb40ff9f554599cc33a3d3c
66Estef <lintzyzep@gmail.com>
2023-12-28 23:46:48 -0600

    PA: Variables

---
+++ FP/Practica1A/Practica1A/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//GARCIA_ZEPEDA_290122
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Práctica A: Variables Sencillas");
+
+//Variables para mostrar los datos ingresados
+string nombre = "Lizzet";
+string surname = "Zepeda";
+string account = "28759364";
+string field = "Animación";
+string email = "@lizep.com/art";
+string celnum = "5556700011";
+string subject = "Fundamentos de Programación";
+int cuatri = 6;
+
+//Muestra de datos en consola
+
+Console.WriteLine("Nombre del Usuario:" + nombre);
+Console.WriteLine("Apellido del Usuario:" + surname);
+Console.WriteLine("Número de Cuenta:" + account);
+Console.WriteLine("Carrera Universitaria:" + field);
+Console.WriteLine("Correo de contacto:" + email);
+Console.WriteLine("Número telefónico de contacto:" + celnum);
+Console.WriteLine("Materia que cursa:" + subject);
+Console.WriteLine("Cuatrimestre en curso:" + cuatri);
